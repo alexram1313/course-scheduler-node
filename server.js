72194345ba406898c0bda98d3aa1a0a96ddeadb7
Alex I. Ramirez <alex@arcompware.com>
2017-06-09 16:52:39 -0700

    About page change

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/', function (req, res) {
 
 app.get('/about', function (req, res) {
   var title = "About";
-  var content = "<p>ZotScheduler is a tool that generates class schedules for students. Eventually, this about page will be more interesting. Developed by David Legg and Alex I. Ramirez.</p>";
+  var content = "<p>Born out of a poor anteater's frustration with scheduling courses, David Legg, along with his partner in crime Alex I. Ramirez, have developed ZotScheduler - Course Scheduler. ZotScheduler is a tool that significantly simplifies WebSOC for you and gives you all possible class combinations.</p>";
   res.render('stdpage', {
     "title": title,
     "content": content
